9885c4ce049f1c8a53dc5cd355375a14acfc22cb
MadeehaMinhas <minhas.madeeha97@gmail.com>
2025-06-08 16:44:16 +0800

    Replaced project with updated version

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from app.socratic_ai import generate_response
+import os
 
 app = FastAPI()
 
@@ -13,10 +17,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+templates = Jinja2Templates(directory="app/frontend")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 @app.post("/ask")
 async def ask_question(request: Request):
     data = await request.json()
-    print("Received question:", data)
     user_input = data.get("question", "")
     try:
         response = generate_response(user_input)
